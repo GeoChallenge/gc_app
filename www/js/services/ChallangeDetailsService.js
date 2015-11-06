5dd2a2ad96799b5c040c96a65b8b9a6718444ab4
TheJetlag <TheJetlag@users.noreply.github.com>
2015-11-06 23:49:56 +0100

    remove unused stuff

---
+++ www/js/services/ChallangeDetailsService.js
@@ -0,0 +1,15 @@
+angular.module('gc.services')
+
+.service('ChallengesOverview', function($http, $q, $timeout) {
+
+    /*
+     * Returns the metadata of all challanges
+     */
+    var loadChallangeOverview = function() {
+
+    };
+
+    return {
+        
+    };
+});
