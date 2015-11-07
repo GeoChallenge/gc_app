e6eb41eef77782c2c52391693520afb8161ceb81
TheJetlag <TheJetlag@users.noreply.github.com>
2015-11-07 09:09:11 +0100

    wip

--- www/js/controllers/ChallengeSignupController.js
+++ www/js/controllers/ChallengeSignupController.js
@@ -1,6 +1,17 @@
 angular.module('gc.controllers')
 
-.controller('ChallengeSignupController', function($scope, $stateParams) {
-    $scope.id = $stateParams.id;
+.controller('ChallengeSignupController', function($scope, $stateParams, ChallengesDetails) {
+
+    $scope.$on('$ionicView.beforeEnter', function(){
+        $scope.id = $stateParams.id;
+
+        ChallengesDetails.loadChallangeDetails($stateParams.id).then(function(challenge){
+            $scope.challenge = challenge;
+        }, function(){
+            // error
+        }).finally(function(){
+            // finally
+        });
+    });
 
 });
---
+++ www/js/services/ChallengeDetailsService.js
@@ -0,0 +1,30 @@
+angular.module('gc.services')
+
+.service('ChallengesDetails', function($http, $q, $timeout) {
+
+    /*
+     * Returns the metadata of all challanges
+     */
+    var loadChallangeDetails = function(id) {
+        var deferred = $q.defer();
+
+        $timeout(function() {
+            var challengeDetail = {
+                    title: "Schitzeljagd I",
+                    challangeId: id,
+                    startLocationDescription: "Königsstraße",
+                    startDate: "2015-11-06T20:29:12.485Z",
+                    signedUser: 5,
+                    approxDuration: "5h",
+                    minParticipants: 5,
+                };
+            deferred.resolve(challengeDetail);
+        }, 500);
+
+        return deferred.promise;
+    };
+
+    return {
+        loadChallangeDetails: loadChallangeDetails
+    };
+});
