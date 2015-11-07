3945519e35ab8fea24909bdd56f7402a14aff77b
TheJetlag <TheJetlag@users.noreply.github.com>
2015-11-07 20:00:06 +0100

    wip

--- www/js/controllers/runningMyHistoryController.js
+++ www/js/controllers/runningMyHistoryController.js
@@ -35,6 +35,19 @@ angular.module('starter').controller('RunningMyHistoryController',
 
         });
 
+        $scope.historyPath = {
+            p1: {
+                color: 'red',
+                weight: 8,
+                latlngs: [
+                    { lat: 51.50, lng: -0.082 },
+                    { lat: 48.83, lng: 2.37 },
+                    { lat: 41.91, lng: 12.48 }
+                ],
+                message: "<h3>Route from London to Rome</h3><p>Distance: 1862km</p>",
+            }
+        }
+
 
 
          var Location = function() {
