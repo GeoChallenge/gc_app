8544248b4b752a1e6a62b46c3f53e14e7e7b6c2f
AndreasSTR <32stan1bif@hft-stuttgart.de>
2015-11-07 08:57:42 +0100

    entered Dummys Challenge Details

--- www/js/services/ChallangeDetailsService.js
+++ www/js/services/ChallangeDetailsService.js
@@ -6,7 +6,22 @@ angular.module('gc.services')
      * Returns the metadata of all challanges
      */
     var loadChallangeOverview = function() {
+    	 var deferred = $q.defer();
 
+        $timeout(function() {
+            var challengeDetail = 
+                { 
+					title: "Schitzeljagd I",
+				  	challangeId: "schinteljagd_1",
+				    startLocationDescription: "Königsstraße",
+				    startDate: "2015-11-06T20:29:12.485Z",
+				    signedUser: 5,
+				    approxDuration: "5h",
+				    minParticipants: 5,
+    	},
+            ;
+            deferred.resolve(challenges);
+        }, 500);
     };
 
     return {
