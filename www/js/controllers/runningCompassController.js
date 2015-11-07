824b496f8b5bed0aeec2105f155ffaab511e6ccc
TheJetlag <TheJetlag@users.noreply.github.com>
2015-11-07 18:12:42 +0100

    wip

--- www/js/controllers/runningCompassController.js
+++ www/js/controllers/runningCompassController.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 
-.controller('RunningCompassController', function($scope, $interval, $cordovaGeolocation, CurrentChallenge) {
+.controller('RunningCompassController', function($scope, $interval, $cordovaGeolocation, CurrentChallenge, $cordovaDeviceOrientation) {
     console.log("hello from RunningCompassController");
 
     var watchOptions = {
@@ -17,22 +17,20 @@ angular.module('starter')
         function(position) {
             var lat  = position.coords.latitude;
             var lon = position.coords.longitude;
-            console.log(position);
             $scope.distance = CurrentChallenge.calcDifferenceToNextQuestion(lat, lon);
             if (position.coords.speed !== null) {
                 $scope.speed = position.coords.speed;
             }
 
             if (position.coords.heading !== null) {
+
                 $scope.heading = position.coords.heading;
 
                 // calc ange between me and destination
                 var nextDestAngle = CurrentChallenge.calcAngleToNextQuestion(lat, lon);
-                $scope.needleAngle = Math.floor(nextDestAngle + heading);
+                $scope.needleAngle = (Math.floor(nextDestAngle + position.coords.heading)) % 360;
+                console.log($scope.needleAngle);
             }
-            $scope.heading = position.coords.heading;
         }
     );
-
-
 });
